53d4027aae1b69a0cd17bf1de4cf6801ef24fd31
gokcan <gokcankrc@gmail.com>
2017-11-01 08:15:43 +0300

    hotfix, the prev commit was slightly wrong

---
+++ degisen_dosyalari_tespit_etme_itemi.bat
@@ -0,0 +1,3 @@
+cd C:\Users\GOKCAN\Desktop\Hacker Charger
+git status
+timeout /t -1
--- tum_dosyalari_itme_itemi.bat
+++ tum_dosyalari_itme_itemi.bat
@@ -1,4 +1,5 @@
 cd C:\Users\GOKCAN\Desktop\Hacker Charger
+git status
 git add --all
 git commit -m "hotfix, the prev commit was slightly wrong"
 git push origin master
